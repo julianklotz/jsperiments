1dbcc58b429152589fed9053ee67df6f1f9a7960
Julian Klotz <post@julianklotz.de>
2011-12-10 12:44:04 +0100

    Improving example

--- js/domcache-example.js
+++ js/domcache-example.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
 
-    /**
-    * BAD EXAMPLE WITH A TYPICAL jQuery EVENT HANDLER
-    * Problem: the '#p-second' is queried anytime the headline is clicked.
-    * Though, this is in most cases not necessary. Querying it after the first click
-    * and then caching it is sufficient.
-    **/
-    
+  /**
+  * BAD EXAMPLE WITH A TYPICAL jQuery EVENT HANDLER
+  * Problem: the '#p-second' is queried anytime the headline is clicked.
+  * Though, this is in most cases not necessary. Querying it after the first click
+  * and then caching it is sufficient.
+  **/
+  console.dir('badexample');
+  var time = Date.now();
+  for(var i = 0; i < 10000; i++) {
     $('h1#headline').click(function() {
-        $('#p-second').html('I just changed.');
+      $('#p-second').html('I just changed.');
     });
-    
-    /**
-    * SAME EXAMPLE WITH domCache
-    * It helps you to remove selector strings from you code and put them in a single
-    * object. Moreover, the dom objects are created when first accessed and on all subsequent
-    * requests returned from cache.
-    **/
-    
-    // Selectors needed during program runtime
-    var selectors = {
-        headline:    'h1#headline',
-        pSecond:     '#p-second'
-    }
+  }
+  var diff = Date.now() - time;
+  console.dir(diff);
+  
+  /**
+  * SAME EXAMPLE WITH domCache
+  * It helps you to remove selector strings from you code and put them in a single
+  * object. Moreover, the dom objects are created when first accessed and on all subsequent
+  * requests returned from cache.
+  **/
+  
+  // Selectors needed during program runtime
+  var selectors = {
+    headline:  'h1#headline',
+    pSecond:   '#p-second'
+  }
 
-    // Initialize domCache
-    domCache.setup(selectors);
+  // Initialize domCache
+  domCache.setup(selectors);
+  time = Date.now();
+  for(var i = 0; i < 10000; i++) {
     domCache.headline().click(function() {
-        domCache.pSecond().html('I just changed');
+      domCache.pSecond().html('I just changed');
     })
-    
-
+  }
+  diff = Date.now() - time;
+  console.dir(diff);
 });
--- js/domcache.js
+++ js/domcache.js
@@ -8,27 +8,27 @@
 **/
 
 var domCache = {
-    /**
-    * setup creates functions that return DOM elements dynamically, depending on the selector
-    * @param selectors          {Object}   Object w/ key-value pairs. The keys will be used as function names
-    * @param selectorFunction   {function}  Function to which the selector is passed when the element is requested
-    *                                      for the first time. Default is the jQuery "$" function.
-    * @return                   {void}    
-    **/
-    setup: function(selectors, selectorFunction) {
-        selectorFunction = selectorFunction || $;
-        var domElement;
-        for(key in selectors) {
-            if(selectors.hasOwnProperty(key)) {
-                domCache[key] = function(key, selectors, domElement) {
-                    return function() {
-                        if(typeof domElement == 'undefined') {
-                            domElement = selectorFunction(selectors[key]);
-                        }
-                        return(domElement);
-                    };
-                }(key, selectors, domElement);
+  /**
+  * setup creates functions that return DOM elements dynamically, depending on the selector
+  * @param selectors      {Object}   Object w/ key-value pairs. The keys will be used as function names
+  * @param selectorFunction   {function}  Function to which the selector is passed when the element is requested
+  *                    for the first time. Default is the jQuery "$" function.
+  * @return           {void}  
+  **/
+  setup: function(selectors, selectorFunction) {
+    selectorFunction = selectorFunction || $;
+    var domElement;
+    for(key in selectors) {
+      if(selectors.hasOwnProperty(key)) {
+        domCache[key] = function(key, selectors, domElement) {
+          return function() {
+            if(typeof domElement == 'undefined') {
+              domElement = selectorFunction(selectors[key]);
             }
-        }
+            return(domElement);
+          };
+        }(key, selectors, domElement);
+      }
     }
+  }
 }
