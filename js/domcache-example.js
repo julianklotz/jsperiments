a0999fafca85020ca8fa107f7da53c3cbbcca3d2
Julian Klotz <post@julianklotz.de>
2015-01-21 21:52:47 +0100

    Code cleanup.

--- js/domcache-example.js
+++ js/domcache-example.js
@@ -1,49 +1,19 @@
-var dc;
+/**
+ * EXAMPLE USAGE
+ */
 
 $(document).ready(function() {
-
-  /**
-  * BAD EXAMPLE WITH A TYPICAL jQuery EVENT HANDLER
-  * Problem: the '#p-second' is queried anytime the headline is clicked.
-  * Though, this is in most cases not necessary. Querying it after the first click
-  * and then caching it is sufficient.
-  **/
-  console.log('Test case: Assign a click handler to a DOM element 10.000 times. How long does it take?');
-  var time = Date.now();
-  for(var i = 0; i < 10000; i++) {
-    $('h1#headline').click(function() {
-      $('#p-second').html('I just changed.');
-    });
-  }
-  var diff = Date.now() - time;
-  console.log('Without domCache: ' + diff + "ms");
-  
-  /**
-  * SAME EXAMPLE WITH domCache
-  * It helps you to remove selector strings from you code and put them in a single
-  * object. Moreover, the dom objects are created when first accessed and on all subsequent
-  * requests returned from cache.
-  **/
-  
-  // Selectors needed during program runtime
-  var selectors = {
-    headline:  'h1#headline',
-    pSecond:   '#p-second',
-    'asd asd ': '#id'
-  }
-
-  // Initialize domCache
-  dc = Object.create(domCache);
-  dc.setup(selectors);
-
-  time = Date.now();
-  
-  for(var i = 0; i < 10000; i++) {
-    dc.headline().click(function() {
-      dc.pSecond().html('I just changed');
-    })
-  }
-  diff = Date.now() - time;
-  console.log('With domCache: ' + diff + "ms");
-  console.log('If you want to find out how to use domCache in your code, take a look at js/domcache-example.js');
+	// Define the DOM elements that you need
+	var selectors = {
+		headline: 'h1#headline',
+		pSecond: 'p#second'
+	};
+	
+	// Create an instance of domcache and tell it about your selectors
+	var dc = Object.create(domcache).setup(selectors);
+	
+	// Anytime you’d like to access a DOM element, do this:
+	var myHeadline = dc.headline();
+	// Or:
+	var myParagraph = dc.pSecond();
 });
--- js/domcache.js
+++ js/domcache.js
@@ -1,62 +1,60 @@
 /**
-* A general purpose DOM object caching function
+* domcache helps you separate selectors from code and handles caching for you.
 *
-* Copyright 2011, Julian Klotz 
-* version 1.0
-* date 2011-12-01
+* Copyright 2011, 2015 Julian Klotz
+* Version 1.1
+* Date 2015-01-21
 * Released under the MIT License.
 **/
 
 
-/**
-* Prototypal inheritence in Douglas Crockford style ;-)
-* cp. http://javascript.crockford.com/prototypal.html
-* @param {Object}     o Object that you would like to extend
-* @return {Function}  returns a function object; the returned function’s prototype is linked to
-*                     the object that you supplied as argument to Object.create()
-**/
-if( typeof Object.create !== 'function'){
-  Object.create = function(o){
-    function F() {}
-    F.prototype = o;
-    return new F();
-  };
-}
+(function () {
+	/**
+	* Polyfill for Object.create()
+	**/
+	if(typeof Object.create != 'function'){
+		Object.create = function(o){
+			function F() {}
+			F.prototype = o;
+			return new F();
+		};
+	}
+	window.domCache = {
+		/**
+		* Setup creates functions that return DOM elements dynamically, based on the selectors param
+		* @param selectors {Object}	 Object w/ key-value pairs.
+		* @example
+		* 	Doing this: `var dc = Object.create(domCache); dc.setup({chapterHeadline: 'chapter-headline'})`,
+		* 	you can get the element by calling `dc.chapterHeadline()`
+		**/
+		setup: function(selectors) {
+			if(this.hasOwnProperty('setup')) {
+				// The domcache object is "abstract". This is what happens when you try to call domCache directly:
+				throw new Error('domCache is "abstract". You need to copy it by calling Object.create(domCache) first.');
+			}
 
-var domCache = {
-  /**
-  * setup creates functions that return DOM elements dynamically, depending on the selector
-  * @param selectors      {Object}   Object w/ key-value pairs. The keys will be used as function names
-  * @param selectorFunction   {function}  Function to which the selector is passed when the element is requested
-  *                    for the first time. Default is the jQuery "$" function.
-  * @return           {void}  
-  **/
-  setup: function(selectors, selectorFunction) {
-    // The domcache object is "abstract". This is what happens when you try to call domCache directly:
-    if(this.hasOwnProperty('setup')) {
-      throw new Error('domCache is "abstract". You need to copy it by calling Object.create(domCache) first.');
-    }
-    
-    selectorFunction = selectorFunction || $;
-    var domElement,
-        escapedKey;
-    for(key in selectors) {
-      // check for collisions with method names used by domCache itself, e.g. setup
-      if(typeof this[key] != 'undefined') {
-        throw ('You cannot use "' +  key + '" as selector, because this name is used by domCache internally.');
-      }
-      
-      if(selectors.hasOwnProperty(key)) {
-        escapedKey = encodeURI(key.replace(/\s/g, ''));
-        domCache[escapedKey] = function(key, selectors, domElement) {
-          return function() {
-            if(typeof domElement == 'undefined') {
-              domElement = selectorFunction(selectors[key]);
-            }
-            return(domElement);
-          };
-        }(key, selectors, domElement);
-      }
-    }
-  }
-}
+			var	escapedKey,
+				that = this;
+
+			for(key in selectors) {
+				if(selectors.hasOwnProperty(key)) {
+					escapedKey = encodeURI(key.replace(/\s/g, ''));
+
+					// Create a closure that keeps a reference to its selector.
+					// As soon as the returned function is called, a reference to the DOM element is stored.
+					that[escapedKey] = (function(selector) {
+						var domElement = undefined;
+						return function() {
+							if(typeof domElement == 'undefined') {
+								domElement = $(selector);
+							}
+							return domElement;
+						};
+					})(selectors[key]);
+				}
+			}
+
+			return this
+		}
+	}
+}).call(this);
